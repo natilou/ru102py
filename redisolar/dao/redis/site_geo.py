49312402344787d03ff7e628ebc1e0ed360dbe5e
natilou <30585029+natilou@users.noreply.github.com>
2023-06-30 13:05:58 -0300

    refactor find_all() from SiteGeoDaoRedis to use pipeline - bonus challenge

--- redisolar/dao/redis/site_geo.py
+++ redisolar/dao/redis/site_geo.py
@@ -90,10 +90,12 @@ class SiteGeoDaoRedis(SiteGeoDaoBase, RedisDaoBase):
         """Find all Sites."""
         site_ids = self.redis.zrange(self.key_schema.site_geo_key(), 0, -1)
         sites = set()
-
+        pipeline = self.redis.pipeline(transaction=False)
+        
         for site_id in site_ids:
             key = self.key_schema.site_hash_key(site_id)
-            site_hash = self.redis.hgetall(key)
-            sites.add(FlatSiteSchema().load(site_hash))
+            pipeline.hgetall(key)
 
+        site_hashes = pipeline.execute()
+        sites = {FlatSiteSchema().load(site_hash) for site_hash in site_hashes}
         return sites
